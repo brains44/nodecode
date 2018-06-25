a414626fda4549c890ac3a31868f98cba4881c39
Brian <brains44@googlemail.com>
2018-06-25 21:15:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
 runGetRequest();
-//runDatabaseQuery();
+runDatabaseQuery();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
@@ -29,7 +29,7 @@ function runGetRequest() {
 
 //Executes a SQL query
 function runDatabaseQuery() {
-    connection.query("SELECT * FROM SampleTable", function(error, rows, fields) {
+    connection.query("SELECT * FROM SecretTable", function(error, rows, fields) {
         if(!error) {
             console.log(rows);
         } else {
